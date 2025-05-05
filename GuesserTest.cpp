981e6aee1d4ba4efeeb4b6644e42eca8d201dd53
CharlieThao <charliethao347@hotmail.com>
2025-05-04 20:38:22 -0700

    Add unit testing test suite

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -14,9 +14,78 @@ class GuesserTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-// Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
+
+TEST(GuesserTest, correct_match_test)
 {
   Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+  bool result = object.match("Secret");
+  ASSERT_EQ( 1, result );
+}
+
+TEST(GuesserTest, wrong_match_test)
+{
+  Guesser object("Secret");
+  bool result = object.match("Sekret");
+  ASSERT_EQ( 0, result );
+}
+
+TEST(GuesserTest, locked_forever_no_attempts_left_test)
+{
+  Guesser object("Secret");
+  bool result = object.match("Sekret");
+  result = object.match("Sekret");
+  result = object.match("Sekret");
+  result = object.match("Secret");
+  ASSERT_EQ( 0, result );
+}
+
+TEST(GuesserTest, locked_forever_brute_force_test)
+{
+  Guesser object("Secret");
+  bool result = object.match("Secretttt");
+  result = object.match("Secret");
+  ASSERT_EQ( 0, result );
+}
+
+TEST(GuesserTest, correct_match_multiple_times_test)
+{
+  Guesser object("Secret");
+  bool result = object.match("Secret");
+  result = object.match("Secret");
+  result = object.match("Secret");
+  result = object.match("Secret");
+  ASSERT_EQ( 1, result );
+}
+
+TEST(GuesserTest, attempts_reset_test)
+{
+  Guesser object("Secret");
+  bool result = object.match("Sekret");
+  result = object.match("Sekret");
+  result = object.match("Secret");
+  result = object.match("Sekret");
+  result = object.match("Sekret");
+  result = object.match("Secret");
+  ASSERT_EQ( 1, result );
+}
+
+TEST(GuesserTest, truncate_test)
+{
+  Guesser object("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+  bool result = object.match("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+  ASSERT_EQ( 0, result );
+}
+
+TEST(GuesserTest, case_sensitive_test)
+{
+  Guesser object("Secret");
+  bool result = object.match("secret");
+  ASSERT_EQ( 0, result );
+}
+
+TEST(GuesserTest, empty_string_test)
+{
+  Guesser object("");
+  bool result = object.match("");
+  ASSERT_EQ( 1, result );
 }
